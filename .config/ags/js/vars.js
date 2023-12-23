9de860a075650417719670956361b530cd07b88e
João Gabriel Alves <joao.aac@disroot.org>
2023-12-22 22:03:01 -0300

    não precisa usar o kde

--- .config/ags/js/vars.js
+++ .config/ags/js/vars.js
@@ -16,6 +16,10 @@ const dwlIpc = Variable(JSON.parse(Utils.exec("dwl-msg status")), {
   listen: [["dwl-msg", "follow"], (out) => JSON.parse(out)],
 });
 
+// const dwlIpc = Variable(JSON.parse("[]"), {
+//   listen: [["dwl-msg", "follow"], (out) => JSON.parse(out)],
+// });
+
 // const time = Variable(0, {
 //   poll: [1000, ["date", "+%s"]],
 // });
