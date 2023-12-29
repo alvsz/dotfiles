8760b1b8d119648f72af8f755ec84e54f0f7d369
Joao Gabriel Alves <joao.aac@disroot.org>
2023-12-29 11:29:59 -0300

    sintaxe nova

--- .config/ags/js/notifWidgets.js
+++ .config/ags/js/notifWidgets.js
@@ -4,113 +4,118 @@ import Gtk from "gi://Gtk";
 import Widget from "resource:///com/github/Aylur/ags/widget.js";
 
 const List = () =>
-    Widget.Box({
-        vertical: true,
-        vexpand: true,
-        connections: [
-            [
-                Notifications,
-                (self) => {
-                    self.children = Notifications.notifications
-                        .reverse()
-                        .map(Notification);
-
-                    self.visible = Notifications.notifications.length > 0;
-
-                    self.children.length > 0 &&
-                        self.children[0].toggleClassName("first", true);
-                },
-            ],
-        ],
-    });
+  Widget.Box({
+    vertical: true,
+    vexpand: true,
+    vpack: "fill",
+    children: Notifications.bind("notifications").transform((l) =>
+      l.reverse().map(Notification),
+    ),
+    visible: true,
+    // connections: [
+    //   [
+    //     Notifications,
+    //     (self) => {
+    //       // self.children = Notifications.notifications
+    //       //     .reverse()
+    //       //     .map(Notification);
+    //
+    //       self.visible = Notifications.notifications.length > 0;
+    //
+    //       self.children.length > 0 &&
+    //         self.children[0].toggleClassName("first", true);
+    //     },
+    //   ],
+    // ],
+  });
 
 const Placeholder = () =>
-    Widget.Box({
-        className: "placeholder",
-        vertical: true,
-        vexpand: true,
-        valign: "center",
-        children: [
-            Widget.Icon("notifications-disabled-symbolic"),
-            Widget.Label("Your inbox is empty"),
-        ],
-        binds: [["visible", Notifications, "notifications", (n) => n.length === 0]],
-    });
+  Widget.Box({
+    className: "placeholder",
+    vertical: true,
+    vexpand: true,
+    valign: "center",
+    children: [
+      Widget.Icon("notifications-disabled-symbolic"),
+      Widget.Label("Your inbox is empty"),
+    ],
+    binds: [["visible", Notifications, "notifications", (n) => n.length === 0]],
+  });
 
 export const NotificationList = () =>
-    Widget.Scrollable({
-        className: "scroll",
-        hexpand: true,
-        hscroll: "never",
-        vscroll: "automatic",
-        child: Widget.Box({
-            className: "list",
-            // spacing: 5,
-            vertical: true,
-            children: [List(), Placeholder()],
-        }),
-    });
+  Widget.Scrollable({
+    className: "scroll",
+    hexpand: true,
+    hscroll: "never",
+    vscroll: "automatic",
+    child: Widget.Box({
+      className: "list",
+      // spacing: 5,
+      vertical: true,
+      children: [List(), Placeholder()],
+    }),
+  });
 
 export const ClearButton = () =>
-    Widget.Button({
-        onClicked: () => Notifications.clear(),
-        binds: [["sensitive", Notifications, "notifications", (n) => n.length > 0]],
-        child: Widget.Box({
-            children: [
-                Widget.Label("Clear"),
-                Widget.Icon({
-                    binds: [
-                        [
-                            "icon",
-                            Notifications,
-                            "notifications",
-                            (n) => `user-trash-${n.length > 0 ? "full-" : ""}symbolic`,
-                        ],
-                    ],
-                }),
+  Widget.Button({
+    onClicked: () => Notifications.clear(),
+    binds: [["sensitive", Notifications, "notifications", (n) => n.length > 0]],
+    child: Widget.Box({
+      children: [
+        Widget.Label("Clear"),
+        Widget.Icon({
+          binds: [
+            [
+              "icon",
+              Notifications,
+              "notifications",
+              (n) => `user-trash-${n.length > 0 ? "full-" : ""}symbolic`,
             ],
+          ],
         }),
-    });
+      ],
+    }),
+  });
 
 export const DNDSwitch = () =>
-    Widget.Switch({
-        // type: Gtk.Switch,
-        valign: "center",
-        connections: [
-            [
-                "notify::active",
-                ({ active }) => {
-                    Notifications.dnd = active;
-                },
-            ],
-        ],
-    });
+  Widget.Switch({
+    // type: Gtk.Switch,
+    valign: "center",
+    connections: [
+      [
+        "notify::active",
+        ({ active }) => {
+          Notifications.dnd = active;
+        },
+      ],
+    ],
+  });
 
 export const PopupList = () =>
-    Widget.Box({
-        className: "popupList",
-        // style: "padding: 1px;", // so it shows up
-        spacing: 5,
-        vertical: true,
-        connections: [
-            [
-                Notifications,
-                (self) => {
-                    self.children = Notifications.popups.reverse().map(Notification);
+  Widget.Box({
+    className: "popupList",
+    // style: "padding: 1px;", // so it shows up
+    spacing: 5,
+    vertical: true,
+    connections: [
+      [
+        Notifications,
+        (self) => {
+          self.children = Notifications.popups.reverse().map(Notification);
 
-                    // self.visible = Notifications.notifications.length > 0;
+          // self.visible = Notifications.notifications.length > 0;
 
-                    self.children.length > 0 &&
-                        self.children[0].toggleClassName("first", true);
-                },
-            ],
-        ],
-        // binds: [
-        //     [
-        //         "children",
-        //         Notifications,
-        //         "popups",
-        //         (popups) => popups.map(Notification),
-        //     ],
-        // ],
-    });
+          self.children.length > 0 &&
+            self.children[0].toggleClassName("first", true);
+        },
+      ],
+    ],
+    // binds: [
+    //     [
+    //         "children",
+    //         Notifications,
+    //         "popups",
+    //         (popups) => popups.map(Notification),
+    //     ],
+    // ],
+  });
