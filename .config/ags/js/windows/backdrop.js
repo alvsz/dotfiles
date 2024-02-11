2914397e7fbc61e55a43304930839dc24648a270
João Gabriel Alves <joao.aac@disroot.org>
2024-02-10 22:09:39 -0300

    quero deixar o album quadrado sempre

--- .config/ags/js/windows/backdrop.js
+++ .config/ags/js/windows/backdrop.js
@@ -5,7 +5,7 @@ import GLib from "gi://GLib";
 const clock = () =>
   Widget.Label({
     className: "time",
-    hpack: "center",
+    // hpack: "center",
     connections: [
       [
         30000,
@@ -15,12 +15,18 @@ const clock = () =>
         },
       ],
     ],
+  }).on("draw", (album, cr) => {
+    print("largura:");
+    print(album.get_allocated_width());
+    print("altura:");
+    print(album.get_allocated_height());
+    print("\n\n\n");
   });
 
 const date = () =>
   Widget.Label({
     className: "date",
-    hpack: "center",
+    // hpack: "center",
     connections: [
       [
         30000,
@@ -32,23 +38,42 @@ const date = () =>
     ],
   });
 
+// const albumArt = () =>
+//   Widget.Icon({
+//     className: "albumArt",
+//   }).hook(Mpris, (self) => {
+//     const temp = Mpris.getPlayer("");
+//
+//     if (temp) {
+//       self.icon = temp.coverPath;
+//       self.visible = true;
+//     } else {
+//       self.visible = false;
+//     }
+//   });
+//
 const albumArt = () =>
   Widget.Box({
+    className: "albumArt",
     hpack: "center",
-    children: [
-      Widget.Icon({
-        className: "albumArt",
-      }).hook(Mpris, (self) => {
-        const temp = Mpris.getPlayer("");
-        if (temp) {
-          self.icon = temp.coverPath;
-          self.visible = true;
-        } else {
-          icon.visible = false;
-        }
-      }),
-    ],
-  });
+  })
+    .hook(Mpris, (self) => {
+      const temp = Mpris.getPlayer("");
+
+      if (temp) {
+        self.css = `background-image: url("${temp.coverPath}")`;
+        self.visible = true;
+      } else {
+        self.visible = false;
+      }
+    })
+    .on("draw", (album, cr) => {
+      print("largura:");
+      print(album.get_allocated_width());
+      print("altura:");
+      print(album.get_allocated_height());
+      print("\n\n\n");
+    });
 
 const Backdrop = ({ monitor } = {}) =>
   Widget.Window({
@@ -60,7 +85,7 @@ const Backdrop = ({ monitor } = {}) =>
     child: Widget.Box({
       className: "backdrop",
       vertical: true,
-      hpack: "center",
+      // hpack: "center",
       homogeneous: false,
       children: [
         // clock(),
