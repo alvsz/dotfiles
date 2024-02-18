29c90e08db6844afadd6fe7f101c7148905beb8c
João Gabriel Alves <joao.aac@disroot.org>
2024-02-17 23:58:59 -0300

    é isso aí

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,12 +1,12 @@
 import Bar from "./js/windows/bar.js";
+import AppMenu from "./js/windows/appmenu.js";
+import Calendar from "./js/windows/calendar.js";
+import Backdrop from "./js/windows/backdrop.js";
+
 // import {
 //   NotificationCenter,
 //   // NotificationsPopupWindow,
 // } from "./js/windows/notificationCenter.js";
-
-import AppMenu from "./js/windows/appmenu.js";
-import Calendar from "./js/windows/calendar.js";
-import Backdrop from "./js/windows/backdrop.js";
 // import Lockscreen from "./js/windows/lockscreen.js";
 
 import { cssPath, forMonitors, scssWatcher } from "./js/utils.js";
@@ -14,11 +14,11 @@ import { cssPath, forMonitors, scssWatcher } from "./js/utils.js";
 // import goaClient from "./js/services/goaClient.js";
 // globalThis.goa = goaClient;
 
-import iCal from "./js/services/iCal.js";
-globalThis.ical = iCal;
+// import iCal from "./js/services/iCal.js";
+// globalThis.ical = iCal;
 
-import * as Utils from "resource:///com/github/Aylur/ags/utils.js";
-globalThis.utils = Utils;
+// import * as Utils from "resource:///com/github/Aylur/ags/utils.js";
+// globalThis.utils = Utils;
 
 // import * as vars from "./js/vars.js";
 
