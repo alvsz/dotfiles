a4f801e3e06199c16d38d0d454f7595a3de8ebfb
Joao Gabriel Alves <joao.aac@disroot.org>
2024-05-19 13:37:08 -0300

    nem  seo

--- .config/ags/js/misc/bouncingText.js
+++ .config/ags/js/misc/bouncingText.js
@@ -1,43 +1,31 @@
 import Widget from "resource:///com/github/Aylur/ags/widget.js";
 
 const scrollable = (widget, scrollSpeed) => {
-  let running = true;
   let scrollDirection = 1;
 
   if (!scrollSpeed) {
     scrollSpeed = 200;
   }
 
-  return (
-    Widget.Scrollable({
-      hscroll: "always",
-      vscroll: "never",
-      hscrollbar_policy: 3,
-      hexpand: true,
-      hpack: "fill",
-      className: "bouncingText",
-      child: widget,
-    })
-      .poll(50, (self) => {
-        const hAdjustment = self.get_hadjustment();
-        const currentValue = hAdjustment.get_value();
+  return Widget.Scrollable({
+    hscroll: "always",
+    vscroll: "never",
+    hscrollbar_policy: 3,
+    hexpand: true,
+    hpack: "fill",
+    className: "bouncingText",
+    child: widget,
+  })
+    .poll(50, (self) => {
+      const hAdjustment = self.get_hadjustment();
+      const currentValue = hAdjustment.get_value();
 
-        let newValue = currentValue + scrollDirection * (scrollSpeed / 200); // Adjust the scroll speed by changing this value
-        hAdjustment.set_value(newValue);
-      })
-      // .on("draw", (self) => {
-      // setTimeout(() => {
-      //
-      //   // self.queue_draw();
-      // }, 100);
-      // })
-      .on("edge-reached", () => {
-        scrollDirection *= -1;
-      })
-      .on("destroy", () => {
-        running = false;
-      })
-  );
+      let newValue = currentValue + scrollDirection * (scrollSpeed / 200); // Adjust the scroll speed by changing this value
+      hAdjustment.set_value(newValue);
+    })
+    .on("edge-reached", () => {
+      scrollDirection *= -1;
+    });
 };
 
 export default scrollable;
