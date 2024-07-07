b88b0fa8820218d60be8d5b2824f76f1643ad073
Joao Gabriel Alves <joao.aac@disroot.org>
2024-07-07 14:44:11 -0300

    melhorias

--- .config/ags/js/misc/mediaPlayer.js
+++ .config/ags/js/misc/mediaPlayer.js
@@ -3,6 +3,19 @@ import * as Utils from "resource:///com/github/Aylur/ags/utils.js";
 
 import scrollable from "../misc/bouncingText.js";
 
+const FALLBACK_ICON = "audio-x-generic-symbolic";
+const PLAY_ICON = "media-playback-start-symbolic";
+const PAUSE_ICON = "media-playback-pause-symbolic";
+const PREV_ICON = "media-skip-backward-symbolic";
+const NEXT_ICON = "media-skip-forward-symbolic";
+
+const lengthStr = (length) => {
+  const min = Math.floor(length / 60);
+  const sec = Math.floor(length % 60);
+  const sec0 = sec < 10 ? "0" : "";
+  return `${min}:${sec0}${sec}`;
+};
+
 const mediaPlayer = (player) => {
   const slider = Widget.Slider({
     vertical: false,
@@ -13,18 +26,133 @@ const mediaPlayer = (player) => {
     drawValue: false,
     min: 0,
     max: 1,
+
+    on_change: ({ value }) => (player.position = value * player.length),
+    visible: player.bind("length").as((l) => l > 0),
+
+    setup: (self) => {
+      const update = () => {
+        const value = player.position / player.length;
+        self.value = value > 0 ? value : 0;
+      };
+
+      self.hook(player, update);
+      self.hook(player, update, "position");
+      self.poll(500, update);
+    },
   });
 
   const positionTimer = Widget.Label({
     hpack: "start",
+    vpack: "end",
+    className: "positionTimer",
+    visible: player.bind("length").as((l) => l > 0),
+
+    setup: (self) => {
+      const update = (_, time) => {
+        self.label = lengthStr(time || player.position);
+        self.visible = player.length > 0;
+      };
+
+      self.hook(player, update);
+      self.hook(player, update, "position");
+      self.poll(500, update);
+    },
   });
 
-  const playbackStatus = Widget.Label({
-    label: player.bind("play-back-status"),
-  });
+  // const playbackStatus = Widget.Label({
+  //   label: player.bind("play-back-status"),
+  // });
 
   const lengthTimer = Widget.Label({
     hpack: "end",
+    vpack: "end",
+    className: "lengthTimer",
+    visible: player.bind("length").transform((l) => l > 0),
+    label: player.bind("length").transform(lengthStr),
+  });
+
+  const playPause = Widget.Button({
+    class_name: "play-pause",
+    on_clicked: () => player.playPause(),
+    visible: player.bind("can_play"),
+    child: Widget.Icon({
+      icon: player.bind("play_back_status").transform((s) => {
+        switch (s) {
+          case "Playing":
+            return PAUSE_ICON;
+          case "Paused":
+          case "Stopped":
+            return PLAY_ICON;
+        }
+      }),
+    }),
+  });
+
+  const prev = Widget.Button({
+    on_clicked: () => player.previous(),
+    visible: player.bind("can_go_prev"),
+    child: Widget.Icon(PREV_ICON),
+  });
+
+  const next = Widget.Button({
+    on_clicked: () => player.next(),
+    visible: player.bind("can_go_next"),
+    child: Widget.Icon(NEXT_ICON),
+  });
+
+  const albumCover = Widget.Icon({
+    className: "albumCover",
+  }).hook(player, (self) => {
+    self.visible = player.coverPath != null;
+    self.icon = player.coverPath;
+  });
+
+  const trackTitle = Widget.Box({
+    vertical: false,
+    homogeneous: false,
+    children: [
+      scrollable({
+        child: Widget.Label({
+          label: player.bind("trackTitle"),
+          className: "trackTitle",
+          hexpand: true,
+          hpack: "start",
+        }),
+      }),
+
+      Widget.Icon({
+        className: "playerIcon",
+        hpack: "end",
+      }).hook(player, (self) => {
+        self.tooltipText = player.identity;
+        let icon;
+
+        if (Utils.lookUpIcon(player.name)) {
+          icon = player.name;
+        } else {
+          const palavras = player.identity.split(" ");
+
+          palavras.forEach((element) => {
+            if (Utils.lookUpIcon(element.toLowerCase())) {
+              icon = element;
+            }
+          });
+        }
+
+        if (icon) self.icon = icon;
+        else self.icon = FALLBACK_ICON;
+      }),
+    ],
+  });
+
+  const artists = scrollable({
+    child: Widget.Label({
+      label: player.bind("track_artists").transform((a) => a.join(", ")),
+      className: "artists",
+      hexpand: true,
+      hpack: "start",
+    }),
   });
 
   return Widget.Box({
@@ -32,84 +160,39 @@ const mediaPlayer = (player) => {
     homogeneous: false,
     className: "mediaPlayer",
     children: [
-      Widget.Icon({
-        className: "albumCover",
-      }).hook(player, (self) => {
-        self.visible = player.coverPath != null;
-        self.icon = player.coverPath;
-      }),
+      albumCover,
 
-      Widget.CenterBox({
+      Widget.Box({
         vertical: true,
+        homogeneous: false,
         hpack: "fill",
         vpack: "fill",
-        className: "rightstuff",
-        startWidget: Widget.Box({
-          vertical: false,
-          homogeneous: false,
-          children: [
-            scrollable({
-              child: Widget.Label({
-                label: player.bind("trackTitle"),
-                className: "trackTitle",
+
+        children: [
+          trackTitle,
+          artists,
+          slider,
+          Widget.Box({
+            vertical: false,
+            homogeneous: false,
+            hpack: "fill",
+            vpack: "end",
+            children: [
+              positionTimer,
+
+              Widget.Box({
+                homogeneous: true,
+                vertical: false,
                 hexpand: true,
-                hpack: "start",
+                hpack: "center",
+                vpack: "center",
+                children: [prev, playPause, next],
               }),
-            }),
-            Widget.Icon({
-              className: "playerIcon",
-              hpack: "end",
-            }).hook(player, (self) => {
-              self.tooltipText = player.identity;
 
-              if (Utils.lookUpIcon(player.name)) {
-                self.icon = player.name;
-              } else {
-                const palavras = player.identity.split(" ");
-
-                palavras.forEach((element) => {
-                  if (Utils.lookUpIcon(element.toLowerCase())) {
-                    self.icon = element;
-                  }
-                });
-              }
-            }),
-          ],
-        }),
-
-        centerWidget: slider,
-
-        endWidget: Widget.CenterBox({
-          vertical: false,
-          startWidget: positionTimer,
-          centerWidget: playbackStatus,
-          endWidget: lengthTimer,
-        }),
-      }).poll(500, () => {
-        if (player.length == -1 || player.position == -1) {
-          slider.value = 0;
-          positionTimer.visible = false;
-          lengthTimer.visible = false;
-          return;
-        }
-        positionTimer.visible = true;
-        lengthTimer.visible = true;
-
-        slider.value = player.position / player.length;
-
-        const pmins = Math.floor(player.position / 60);
-        const psecs = Math.floor(player.position - 60 * pmins);
-
-        positionTimer.label = `${String(pmins).padStart(2, "0")}:${String(
-          psecs,
-        ).padStart(2, "0")}`;
-
-        const mins = Math.floor(player.length / 60);
-        const secs = Math.floor(player.length - 60 * mins);
-
-        lengthTimer.label = `${String(mins).padStart(2, "0")}:${String(
-          secs,
-        ).padStart(2, "0")}`;
+              lengthTimer,
+            ],
+          }),
+        ],
       }),
     ],
   });
