59258d20a8ee5194aa2ffef1a2259b6d7266f75d
João Gabriel Alves <joao.aac@disroot.org>
2024-07-14 11:46:38 -0300

    ordenar pela conexão mais forte

--- .config/ags/js/misc/networkInfo.js
+++ .config/ags/js/misc/networkInfo.js
@@ -1,18 +1,85 @@
 import Widget from "resource:///com/github/Aylur/ags/widget.js";
 import Network from "resource:///com/github/Aylur/ags/service/network.js";
 
+import NM from "gi://NM";
+
+const _STRENGTH_ICONS = [
+  { value: 80, icon: "network-wireless-signal-excellent-symbolic" },
+  { value: 60, icon: "network-wireless-signal-good-symbolic" },
+  { value: 40, icon: "network-wireless-signal-ok-symbolic" },
+  { value: 20, icon: "network-wireless-signal-weak-symbolic" },
+  { value: 0, icon: "network-wireless-signal-none-symbolic" },
+];
+
+const get_access_points = () => {
+  return Network.wifi._device
+    .get_access_points()
+    .map((ap) => ({
+      bssid: ap.bssid,
+      address: ap.hw_address,
+      lastSeen: ap.last_seen,
+      ssid: ap.ssid
+        ? NM.utils_ssid_to_utf8(ap.ssid.get_data() || new Uint8Array())
+        : ap.bssid,
+      active: ap === Network.wifi._ap,
+      rsn: ap.rsn_flags,
+      strength: ap.strength,
+      frequency: ap.frequency,
+      iconName: _STRENGTH_ICONS.find(({ value }) => value <= ap.strength)?.icon,
+    }))
+    .sort((b, a) => {
+      return a.strength - b.strength;
+    });
+};
+
 const knownSSIDs = () => {
   let knownSSIDs = new Set();
   const connections = Network._client.get_connections();
 
   for (const connection of connections) {
     const wireless = connection.get_setting_wireless();
-    if (wireless) knownSSIDs.add(wireless.get_ssid().get_data().toString());
+    if (wireless) knownSSIDs.add(wireless.get_ssid().get_data());
   }
   return knownSSIDs;
 };
 
-const wifiButton = (ap, wireless) =>
+const wiredButton = (conn) => {
+  const active = Network.primary === "wired";
+
+  return Widget.Button({
+    className: "networkButton",
+
+    child: Widget.Box({
+      vertical: false,
+      homogeneous: false,
+      spacing: 0,
+      children: [
+        Widget.Icon({
+          className: "icon",
+          icon: conn.iconName,
+        }),
+
+        Widget.Label({
+          className: "networkType",
+          justification: "left",
+          hpack: "start",
+          wrap: false,
+          truncate: "end",
+          hexpand: true,
+          label: "Rede cabeada",
+        }),
+
+        active
+          ? Widget.Icon({
+              icon: "object-select-symbolic",
+            })
+          : null,
+      ],
+    }),
+  });
+};
+
+const wifiButton = (ap) =>
   Widget.Button({
     className: "networkButton",
 
@@ -32,12 +99,15 @@ const wifiButton = (ap, wireless) =>
           hpack: "start",
           wrap: false,
           truncate: "end",
-          label: wireless
-            ? ap.get_ssid()
-              ? ap.get_ssid().get_data().toString()
-              : "Unknown"
-            : "Rede cabeada",
+          hexpand: true,
+          label: ap.ssid,
         }),
+
+        ap.active
+          ? Widget.Icon({
+              icon: "object-select-symbolic",
+            })
+          : null,
       ],
     }),
   });
@@ -47,24 +117,16 @@ const networkList = () => {
   let knownNetworks = [];
   let unknownNetworks = [];
 
-  const aps = Network.wifi._device.get_access_points();
-  // print(aps);
+  const aps = get_access_points();
 
   for (const ap of aps) {
-    // print(ap.get_rsn_flags());
-    // print(ap.get_ssid().get_value());
-    const ssid = ap.get_ssid()?.get_data().toString();
-    // print(ssid);
+    const ssid = ap.ssid;
 
     if (ssid)
       if (SSIDs.has(ssid)) {
-        knownNetworks.push(wifiButton(ap, true));
-        // print(`${ssid} é conhecido`);
-        // print(knownNetworks);
+        knownNetworks.push(wifiButton(ap));
       } else {
-        unknownNetworks.push(wifiButton(ap, true));
-        // print(`${ssid} não é conhecido`);
-        // print(unknownNetworks);
+        unknownNetworks.push(wifiButton(ap));
       }
   }
 
@@ -120,8 +182,6 @@ const info = () => {
         className: "wifiList",
       }).hook(Network.wifi, (self) => {
         const [knownNetworks, unknownNetworks] = networkList();
-        // print(knownNetworks);
-        // print(unkownNetworks);
 
         self.children = [
           Widget.Label({
@@ -130,7 +190,7 @@ const info = () => {
             hpack: "start",
           }),
           Widget.Separator({ vertical: true }),
-          wifiButton(Network.wired, false),
+          wiredButton(Network.wired),
           knownNetworks,
 
           Widget.Label({
