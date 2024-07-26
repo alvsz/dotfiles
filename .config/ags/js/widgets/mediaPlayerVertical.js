491988d4529a8757da756623c3da7bea27037669
Joao Gabriel Alves <joao.aac@disroot.org>
2024-07-26 17:51:40 -0300

    faltou comitar isso

---
+++ .config/ags/js/widgets/mediaPlayerVertical.js
@@ -0,0 +1,155 @@
+import Widget from "resource:///com/github/Aylur/ags/widget.js";
+
+const lengthStr = (length) => {
+  const min = Math.floor(length / 60);
+  const sec = Math.floor(length % 60);
+  const sec0 = sec < 10 ? "0" : "";
+  return `${min}:${sec0}${sec}`;
+};
+
+const mpris = (player) => {
+  const albumCover = Widget.Box({
+    className: "albumArt",
+    hpack: "center",
+    css: player
+      .bind("coverPath")
+      .as((path) => `background-image: url("${path}")`),
+  });
+
+  const slider = Widget.Slider({
+    vertical: false,
+    className: "position",
+    vpack: "center",
+    hexpand: true,
+
+    drawValue: false,
+    min: 0,
+    max: 1,
+
+    on_change: ({ value }) => (player.position = value * player.length),
+    visible: player.bind("length").as((l) => l > 0),
+
+    setup: (self) => {
+      const update = () => {
+        const value = player.position / player.length;
+        self.value = value > 0 ? value : 0;
+      };
+
+      self.hook(player, update);
+      self.hook(player, update, "position");
+      self.poll(500, update);
+    },
+  });
+
+  const positionTimer = Widget.Label({
+    hpack: "start",
+    vpack: "end",
+    className: "positionTimer",
+    visible: player.bind("length").as((l) => l > 0),
+
+    setup: (self) => {
+      const update = (_, time) => {
+        self.label = lengthStr(time || player.position);
+        self.visible = player.length > 0;
+      };
+
+      self.hook(player, update);
+      self.hook(player, update, "position");
+      self.poll(500, update);
+    },
+  });
+
+  const lengthTimer = Widget.Label({
+    hpack: "end",
+    vpack: "end",
+    className: "lengthTimer",
+    visible: player.bind("length").transform((l) => l > 0),
+    label: player.bind("length").transform(lengthStr),
+  });
+
+  const playPause = Widget.Button({
+    class_name: "play-pause",
+    on_clicked: () => player.playPause(),
+    visible: player.bind("can_play"),
+    child: Widget.Icon({
+      icon: player.bind("play_back_status").transform((s) => {
+        switch (s) {
+          case "Playing":
+            return icons.mpris.pause;
+          case "Paused":
+          case "Stopped":
+            return icons.mpris.play;
+        }
+      }),
+    }),
+  });
+
+  const prev = Widget.Button({
+    on_clicked: () => player.previous(),
+    visible: player.bind("can_go_prev"),
+    child: Widget.Icon(icons.mpris.prev),
+  });
+
+  const next = Widget.Button({
+    on_clicked: () => player.next(),
+    visible: player.bind("can_go_next"),
+    child: Widget.Icon(icons.mpris.next),
+  });
+
+  const trackTitle = Widget.Label({
+    label: player.bind("trackTitle"),
+    className: "trackTitle",
+    hexpand: true,
+    justification: "center",
+    truncate: "middle",
+    hpack: "center",
+  });
+
+  const artists = Widget.Label({
+    label: player.bind("track_artists").transform((a) => a.join(", ")),
+    className: "artists",
+    hexpand: true,
+    // hpack: "center",
+    justification: "center",
+    truncate: "middle",
+    hpack: "center",
+    visible: player.bind("track_artists").transform((a) => a != ""),
+  });
+
+  return Widget.Box({
+    vertical: true,
+    homogeneous: false,
+    hexpand: true,
+    hpack: "center",
+    className: "mediaPlayer",
+    children: [
+      albumCover,
+
+      trackTitle,
+      artists,
+      slider,
+      Widget.Box({
+        vertical: false,
+        homogeneous: false,
+        hpack: "fill",
+        vpack: "end",
+        children: [
+          positionTimer,
+
+          Widget.Box({
+            homogeneous: true,
+            vertical: false,
+            hexpand: true,
+            hpack: "center",
+            vpack: "center",
+            children: [prev, playPause, next],
+          }),
+
+          lengthTimer,
+        ],
+      }),
+    ],
+  });
+};
+
+export default mpris;
