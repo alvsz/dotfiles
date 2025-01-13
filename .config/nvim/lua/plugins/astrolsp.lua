939b0d02dc3f0fe90fe705030bbe5ca40a33d86b
Joao Gabriel Alves <joao.aac@disroot.org>
2025-01-13 12:23:26 -0300

    nao formatar com ts_ls

--- .config/nvim/lua/plugins/astrolsp.lua
+++ .config/nvim/lua/plugins/astrolsp.lua
@@ -26,6 +26,7 @@ return {
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         "lua_ls",
         "tsserver",
+        "ts_ls",
         "cssls",
       },
       timeout_ms = 1000, -- default format timeout
