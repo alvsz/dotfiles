94eb79e94aa935697c9050ac595aa8929c1dc5ac
Joao Gabriel Alves <joao.aac@disroot.org>
2025-02-07 16:49:27 -0300

    mpris player list

---
+++ .config/astal/widget/dwlBox.ts
@@ -0,0 +1,51 @@
+import { GLib, exec, subprocess } from "astal";
+import { App, Astal, Gdk, Gtk } from "astal/gtk4";
+import { property, register } from "astal/gobject";
+import template from "./dwlBox.blp";
+
+const ntags = 7;
+const proc = subprocess("dwlcmd follow");
+
+@register({
+  GTypeName: "DwlBox",
+  Template: template,
+  InternalChildren: [
+    "client_box",
+    "client_icon",
+    "client_title",
+    "tags",
+    "layout_button",
+  ],
+})
+export default class dwlBox extends Gtk.Box {
+  declare _client_icon: Gtk.Image;
+  declare _client_title: Gtk.Label;
+  declare _client_box: Gtk.Box;
+  declare _tags: Gtk.Box;
+  declare _layout_button: Gtk.Button;
+  @property(Gdk.Monitor) declare monitor: Gdk.Monitor;
+
+  constructor() {
+    super();
+
+    proc.connect("stdout", () => {
+      const status = exec("dwlcmd run 'return get_status()'");
+      const s = JSON.parse(status);
+
+      const mon = s.find((m: any) => (m.name = this.monitor.get_connector()));
+      const focused = mon.clients.find((c: any) => c.focused);
+
+      if (focused) {
+        this._client_box.visible = true;
+        this._client_icon.icon_name = focused.app_id;
+        this._client_title.label = focused.title;
+
+        this._client_box.tooltip_text = `${focused.title} - ${focused.app_id}`;
+      } else {
+        this._client_box.visible = false;
+      }
+
+      this._layout_button.label = mon.layout;
+    });
+  }
+}
---
+++ .config/astal/widget/mprisPlayerList.ts
@@ -0,0 +1,115 @@
+import { Gdk, Gtk } from "astal/gtk4";
+import GObject, { property, register } from "astal/gobject";
+
+import Mpris from "gi://AstalMpris";
+import template from "./mprisPlayerList.blp";
+import icons from "../icons";
+
+@register({
+  GTypeName: "mprisPlayer",
+  Template: template,
+  InternalChildren: ["previous", "playpause", "next"],
+})
+class mprisPlayer extends Gtk.Box {
+  @property(Mpris.Player) declare player: Mpris.Player;
+  declare _previous: Gtk.Button;
+  declare _playpause: Gtk.Button;
+  declare _next: Gtk.Button;
+
+  constructor(player: Mpris.Player) {
+    super({ player: player } as any);
+  }
+
+  protected format_play_button() {
+    if (this.player.playback_status == Mpris.PlaybackStatus.PLAYING)
+      return icons.mpris.pause;
+    else return icons.mpris.play;
+  }
+
+  protected on_value_changed(self: Gtk.Adjustment) {
+    if (Math.abs(self.value - this.player.position) > 1) {
+      print("setando posição, importante!!!");
+      this.player.set_position(self.value);
+    }
+  }
+
+  protected format_length() {
+    return this.lengthStr(this.player.length);
+  }
+
+  protected format_position() {
+    return this.lengthStr(this.player.position);
+  }
+
+  protected on_previous() {
+    this.player.previous();
+  }
+
+  protected on_playpause() {
+    this.player.play_pause();
+  }
+
+  protected on_next() {
+    this.player.next();
+  }
+
+  private lengthStr(length: number) {
+    if (length === -1) return "--:--";
+    const min = Math.floor(length / 60);
+    const sec = Math.floor(length % 60);
+    const sec0 = sec < 10 ? "0" : "";
+    return `${min}:${sec0}${sec}`;
+  }
+}
+
+@register({
+  GTypeName: "mprisPlayerList",
+})
+export default class mprisPlayerList extends Gtk.Box {
+  declare players: Map<string, Gtk.Box>;
+  @property(Mpris.Mpris) declare mpris: Mpris.Mpris;
+
+  constructor() {
+    super({
+      orientation: Gtk.Orientation.VERTICAL,
+      visible: false,
+    });
+    this.add_css_class("mpris-list");
+    this.mpris = Mpris.get_default();
+    this.players = new Map<string, Gtk.Box>();
+
+    this.mpris.connect(
+      "player-added",
+      (self: Mpris.Mpris, player: Mpris.Player) => {
+        this.add_player(player);
+      },
+    );
+
+    this.mpris.connect(
+      "player-closed",
+      (self: Mpris.Mpris, player: Mpris.Player) => {
+        if (!this.players.has(player.bus_name)) return;
+
+        const widget = this.players.get(player.bus_name);
+        if (widget) {
+          this.players.delete(player.bus_name);
+          this.remove(widget);
+        }
+        if (this.players.size == 0) this.hide();
+      },
+    );
+
+    this.mpris.players.forEach((p) => {
+      this.add_player(p);
+    });
+  }
+
+  private add_player(player: Mpris.Player) {
+    if (this.players.has(player.bus_name)) return;
+
+    const widget = new mprisPlayer(player);
+    this.players.set(player.bus_name, widget);
+    this.append(widget);
+    this.show();
+  }
+}
